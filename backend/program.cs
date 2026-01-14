03701fc2870b609335ef381a33e66f46e8426b97
Firsoova08 <foxalishca@mail.ru>
2026-01-14 09:11:29 +0300

    Added basic backend program

--- backend/program.cs
+++ backend/program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Яшин А.А.");
+Console.WriteLine("Яшин А.А.   ");
 Console.WriteLine(DateTime.Now);
